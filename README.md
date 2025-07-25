0c658fadf9ed7cf0cc1d2ead637312b820a7ceb7
ElavazhaganSF4422 <161409140+ElavazhaganSF4422@users.noreply.github.com>
2025-07-25 17:15:26 +0530

    KB-6537 -How to export DetailsView records in expanded state from WPF DataGrid

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# How-to-export-DetailsView-records-in-expanded-state-from-WPF-DataGrid
-How to export DetailsView records in expanded state from WPF DataGrid
+# How to export DetailsView records in expanded state from WPF DataGrid
+
+You can export [WPF DataGrid](https://www.syncfusion.com/wpf-controls/datagrid) (SfDataGrid) to excel and it is possible to process the exported excel file using XlsIO. For example, you can expand the details view records in exported excel file alone by calling Range.ExpandGroup method for worksheet.
+
+```C#
+ExcelExportingOptions exportoption = new ExcelExportingOptions();
+var excelengine = datagrid.ExportToExcel(datagrid.View, exportoption);
+var workbook = excelengine.Excel.Workbooks[0];
+workbook.Worksheets[0].Range.ExpandGroup(ExcelGroupBy.ByRows, ExpandCollapseFlags.IncludeSubgroups);
+```
+
+In the below screen shot, DetailsView DataGrid expanded only in excel file and not in SfDataGrid.
+
+![Screenshot shown in DetailsView Expanded state](DetailsViewExpanded.png)
+
---
+++ SfDataGridDemo/DataGridWPFSample/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using Syncfusion.UI.Xaml.Grid.Converter;
+using Syncfusion.XlsIO;
+using System.IO;
+using System.Windows;
+
+namespace DataGridWPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ExportToExcelOnClicked(object sender, RoutedEventArgs e)
+        {
+            ExcelExportingOptions exportoption = new ExcelExportingOptions();
+            var excelengine = dataGrid.ExportToExcel(dataGrid.View, exportoption);
+            var workbook = excelengine.Excel.Workbooks[0];
+            workbook.Worksheets[0].Range.ExpandGroup(ExcelGroupBy.ByRows, ExpandCollapseFlags.IncludeSubgroups);
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                FilterIndex = 2,
+                Filter = "Excel 97 to 2003 Files(*.xls)|*.xls|Excel 2007 to 2010 Files(*.xlsx)|*.xlsx|Excel 2013 File(*.xlsx)|*.xlsx"
+            };
+            if (sfd.ShowDialog() == true)
+            {
+                using (Stream stream = sfd.OpenFile())
+                {
+                    if (sfd.FilterIndex == 1)
+                        workbook.Version = ExcelVersion.Excel97to2003;
+                    else if (sfd.FilterIndex == 2)
+                        workbook.Version = ExcelVersion.Excel2010;
+                    else
+                        workbook.Version = ExcelVersion.Excel2013;
+                    workbook.SaveAs(stream);
+                }
+                if (MessageBox.Show("Do you want to view the workbook?", "Workbook has been created", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                {
+                    System.Diagnostics.Process.Start(sfd.FileName);
+                }
+            }
+        }
+    }
+}
